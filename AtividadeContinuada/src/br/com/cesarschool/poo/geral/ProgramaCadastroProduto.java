393a4b839224a5c251bdf28a2538beac9ad0fd1d
Vitor <vitor.velozo2003@gmail.com>
2022-09-21 21:40:54 -0300

    remove from main dir

--- AtividadeContinuada/src/br/com/cesarschool/poo/geral/ProgramaCadastroProduto.java
+++
@@ -1,9 +0,0 @@
-package br.com.cesarschool.poo.geral;
-
-public class ProgramaCadastroProduto {
-
-	public static void main(String[] args) {
-		TelaProduto tela = new TelaProduto();
-		tela.executaTela();
-	}
-}
